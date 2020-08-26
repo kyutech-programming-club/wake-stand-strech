e9a76dce2e28dc9f91f9acf0d318ff90b9447d0a
syu-kwsk <wataru1165kwsk@gmail.com>
2020-08-27 00:20:40 +0900

    Transition wakeUp to standUp in StopAlarmHome

--- lib/StopAlarmHome.dart
+++ lib/StopAlarmHome.dart
@@ -8,13 +8,35 @@ class StopAlarmHome extends StatefulWidget {
 }
 
 class _StopAlarmHomeState extends State<StopAlarmHome> {
+  var _mode = "";
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _mode = "wakeUp";
+    });
+  }
+
+  changeMode(mode) {
+    switch (mode) {
+      case "wakeUp":
+        setState(() {
+          _mode = "standUp";
+        });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "Good Morning!\nLet's stand & stretch!",
-      ),
+      child:
+      _mode == "wakeUp" ?
+      RaisedButton(
+        child: Text("Stop Alarm!\nGo to Stretch"),
+        onPressed: () => changeMode(_mode),
+      ):
+      Text("This is standUp mode page."),
     );
   }
 }
